03a2306a076d066aa2f53f1d3e8c6962aa402087
Aleksandr Golubev aka gulkinnos <gulkinnos@gmail.com>
2017-07-02 20:27:02 +0300

    f#1 Первыя страница проекта

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<html><head>
+<html>
+    <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,7 +16,6 @@
         <script src="/resources/js/jquery-3.1.1.min.js"></script>
         <script src="/resources/js/bootstrap.min.js"></script>
     </head>
-
     <body>
         <div class="top-menu-block row">
             <nav class="navbar navbar-default">
@@ -25,27 +25,40 @@
                             <li class="active">
                                 <a>Главная</a>
                             </li>
+                            <li>
+                                <a href="/about.php">О нас</a>
+                            </li>
+                            <li>
+                                <a href="/contacts.php">Контакты</a>
+                            </li>
+                            <li>
+                                <a href="/prices.php">Стоимость услуг</a>
+                            </li>
+                            <li>
+                                <a href="/results.php">Наши работы</a>
+                            </li>
                         </ul>
                     </div>
                 </div>
             </nav>
         </div>
-        <h1>
-            velo-servis.ru - мастерская по ремонту велосипедов
-        </h1>
-        <p>
-            Уважаемые клиенты, наш сайт находится в стадии разработки.<br>
-            Скоро здесь появится много информации о нашей мастерской, перечень и стоимость услуг, наши работы и другая полезная информация.
-        </p>
+        <h1>velo-servis.ru - вело-мастерская</h1>
+        <div class="logo col-xs-12">
+            <div class="row">
+                <div class="col-xs-11">
+                    <img src="android-chrome-192x192.png" width="64" height="64" alt="мастерская velo-servis.ru"/>
+                </div>
+                <div class="col-xs-10"></div>
+            </div>
+        </div>
+        <p>Уважаемые клиенты, наш сайт находится в стадии разработки.<br>
+            Скоро здесь появится много информации о нашей мастерской, перечень и стоимость услуг, наши работы и другая полезная информация.</p>
         <p>Если у вас возникли вопросы, звоните по телефону <br>+7 (495) 664-35-46
-
         </p>
         <p>Наш адрес: Москва, Школьная улица, 23<br>
             <a href="https://yandex.ru/maps/213/moscow/?source=wizbiz_new_map_single&text=Velo-servis.ru&ll=37.671828%2C55.745667&sll=37.671828%2C55.745667&z=17&sspn=0.016158%2C0.004805&mode=search&ol=biz&oid=156701208291&sctx=CQAAAAEAs02aainWQkCLqMIpbN9LQADg%2F%2F%2Bfi4A%2FAEAebJKuYz8AAAAABQAAAAAAAAABVgt%2BMVnMchnb1gEAAQAAgD8AAAAAAAAAAAIAAABydQ%3D%3D" target="_blank">
                 Мы на Яндекс.Картах                
             </a>
-
         </p>
-
     </body>
 </html>
