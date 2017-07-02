d73c23ff9f409893cf0851eb015dcbfef5f0053f
fast-cat <fast-cat@fast-cat-home>
2017-07-02 17:36:48 +0300

    f#1 Первыя страница проекта

---
+++ index.php
@@ -0,0 +1,46 @@
+<html><head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Мастерская по ремонту велосипедов velo-servis.ru">
+        <title>Мастерская по ремонту велосипедов velo-servis.ru</title>
+        <link rel="shortcut icon" href="/favicon.ico">
+        <link href="/resources/css/bootstrap.min.css" rel="stylesheet">
+        <link href="/resources/css/custom.css" rel="stylesheet">
+        <script src="/resources/js/jquery-3.1.1.min.js"></script>
+        <script src="/resources/js/bootstrap.min.js"></script>
+    </head>
+
+    <body>
+        <div class="top-menu-block row">
+            <nav class="navbar navbar-default">
+                <div class="container-fluid"> 
+                    <div class="navbar-header">
+                        <ul class="nav navbar-nav">
+                            <li class="active">
+                                <a>Главная</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <h1>
+            velo-servis.ru - мастерская по ремонту велосипедов
+        </h1>
+        <p>
+            Уважаемые клиенты, наш сайт находится в стадии разработки.<br>
+            Скоро здесь появится много информации о нашей мастерской, перечень и стоимость услуг, наши работы и другая полезная информация.
+        </p>
+        <p>Если у вас возникли вопросы, звоните по телефону <br>+7 (495) 664-35-46
+
+        </p>
+        <p>Наш адрес: Москва, Школьная улица, 23<br>
+            <a href="https://yandex.ru/maps/213/moscow/?source=wizbiz_new_map_single&text=Velo-servis.ru&ll=37.671828%2C55.745667&sll=37.671828%2C55.745667&z=17&sspn=0.016158%2C0.004805&mode=search&ol=biz&oid=156701208291&sctx=CQAAAAEAs02aainWQkCLqMIpbN9LQADg%2F%2F%2Bfi4A%2FAEAebJKuYz8AAAAABQAAAAAAAAABVgt%2BMVnMchnb1gEAAQAAgD8AAAAAAAAAAAIAAABydQ%3D%3D" target="_blank">
+                Мы на Яндекс.Картах                
+            </a>
+
+        </p>
+
+    </body>
+</html>
