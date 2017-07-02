f226cf5d6f6f7061cea355498892c01fca4dcbe8
Aleksandr Golubev aka gulkinnos <gulkinnos@gmail.com>
2017-07-02 20:26:50 +0300

    f#8 Страница "Наши работы"

---
+++ results.php
@@ -0,0 +1,61 @@
+<html><head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Завершённые работы мастерской velo-servis.ru">
+        <title>Наши работы velo-servis.ru</title>
+        <link rel="shortcut icon" href="favicon.ico">
+        <link href="/resources/css/bootstrap.min.css" rel="stylesheet">
+        <link href="/resources/css/custom.css" rel="stylesheet">
+        <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+        <link rel="manifest" href="manifest.json">
+        <link rel="mask-icon" href="safari-pinned-tab.svg" color="#5bbad5">
+        <script src="/resources/js/jquery-3.1.1.min.js"></script>
+        <script src="/resources/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="top-menu-block row">
+            <nav class="navbar navbar-default">
+                <div class="container-fluid"> 
+                    <div class="navbar-header">
+                        <ul class="nav navbar-nav">
+                            <li>
+                                <a href="/">Главная</a>
+                            </li>
+                            <li>
+                                <a href="/about.php">О нас</a>
+                            </li>
+                            <li>
+                                <a href="/contacts.php">Контакты</a>
+                            </li>
+                            <li>
+                                <a href="/prices.php">Стоимость услуг</a>
+                            </li>
+                            <li class="active">
+                                <a>Наши работы</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <h1>Результаты работы мастерской velo-servis.ru</h1>
+        <div class="logo col-xs-12">
+            <div class="col-xs-4"></div>
+            <div class="col-xs-1">
+                <img src="android-chrome-192x192.png" width="64" height="64" alt="мастерская velo-servis.ru"/>
+            </div>
+            <div class="col-xs-7"></div>
+        </div>
+        <p>Уважаемые клиенты, наш сайт находится в стадии разработки.<br>
+            Скоро здесь появятся примеры наших работ по ремонту велосипедов, самокаов и роликовых коньков.</p>
+        <p>Если у вас возникли вопросы, звоните по телефону <br>+7 (495) 664-35-46</p>
+        <p>Наш адрес: Москва, Школьная улица, 23<br>
+            <a href="https://yandex.ru/maps/213/moscow/?source=wizbiz_new_map_single&text=Velo-servis.ru&ll=37.671828%2C55.745667&sll=37.671828%2C55.745667&z=17&sspn=0.016158%2C0.004805&mode=search&ol=biz&oid=156701208291&sctx=CQAAAAEAs02aainWQkCLqMIpbN9LQADg%2F%2F%2Bfi4A%2FAEAebJKuYz8AAAAABQAAAAAAAAABVgt%2BMVnMchnb1gEAAQAAgD8AAAAAAAAAAAIAAABydQ%3D%3D" target="_blank">
+                Мы на Яндекс.Картах                
+            </a>
+        </p>
+    </body>
+</html>
