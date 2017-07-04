a16bcc8ec1097d5201b2d1ab72162b21345616f8
Aleksandr Golubev <gulkinnos@gmail.com>
2017-07-04 11:00:20 +0300

    Update about.php

--- about.php
+++ about.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="О мастерской velo-servis.ru">
         <title>О мастерской velo-servis.ru</title>
-        <link rel="shortcut icon" href="favicon.ico">
+        <link rel="shortcut icon" href="http://velo-servis.ru/favicon.ico" type="image/x-icon">
         <link href="/resources/css/bootstrap.min.css" rel="stylesheet">
         <link href="/resources/css/custom.css" rel="stylesheet">
         <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
