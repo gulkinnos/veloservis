02d602f16e4a08bddcc75ec4d8badbf6b53cc137
Liya <Home@Lenovo-PC>
2017-07-26 20:53:45 +0300

    исправленная форма POST

--- liyaPost.php
+++ liyaPost.php
@@ -22,12 +22,12 @@ if (isset($_POST['file_input'])) {
     <script src="/resources/js/jquery-3.1.1.min.js"></script>
     <script src="/resources/js/bootstrap.min.js"></script>
 </head>
-<form method="post" action="liyaPost.php"> 
+<form method="post" action="liyaPost.php">
     <textarea name="textarea_input" cols="50" rows="10" required><?php if (isset($_POST['textarea_input'])) echo $textarea_input; ?></textarea> <br> <br> 
     <label><input name="checkbox_input" type="checkbox" value="137" <?php if (isset($_POST['checkbox_input'])) echo 'checked'; ?> >137</label> <br> <br> 
     <label><input name="radiobutton_input" type="radio" value="345" <?php if (isset($_POST['radiobutton_input']) && ($_POST['radiobutton_input']== "345")) echo 'checked'; ?>> 345 </label>
     <label><input name="radiobutton_input" type="radio" value="765" <?php if (isset($_POST['radiobutton_input']) && ($_POST['radiobutton_input']== "765")) echo 'checked'; ?>> 765 </label><br> <br> 
-    <select name="select_input" size = “1” > 
+    <select name="select_input" size = “1”> 
     <option <?php if (isset($_POST['select_input'])&& ($_POST['select_input']== "Полиморфизм")) echo 'selected'; ?> value="Полиморфизм">Полиморфизм</option> 
     <option <?php if (isset($_POST['select_input'])&& ($_POST['select_input']== "Инкапсуляция")) echo 'selected'; ?> value="Инкапсуляция">Инкапсуляция</option> 
     <option <?php if (isset($_POST['select_input'])&& ($_POST['select_input']== "Наследование")) echo 'selected'; ?> value="Наследование">Наследование</option> 
