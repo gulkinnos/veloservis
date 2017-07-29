fac89eff69b13dff73479ba354251911c6bc10a1
Aleksandr Golubev aka gulkinnos <gulkinnos@gmail.com>
2017-07-29 14:15:31 +0300

    Предложения по форме авторизации

--- adminvhod.php
+++ adminvhod.php
@@ -32,31 +32,44 @@ class AuthClass {
 }
 
 $auth = new AuthClass();
-
-if (isset($_POST["login"]) && isset($_POST["password"])) {
-    if (!$auth->auth($_POST["login"], $_POST["password"])) {
-        echo "<h2>Логин или пароль введен не правильно!</h2>";
-    }
-}
-
-if (isset($_GET["logout"])) {
-    if ($_GET["logout"] == 1) {
-        $auth->out();
-        header("Location: ?logout=0");
-    }
-}
-
+//сначала проверяем, авторизован ли пользователь уже?
+//если он авторизован, зачем проверять всё остальное?
 if ($auth->isAuth()) {
+    //разлогиниться можно только если уже залогинен
+    if (isset($_GET["logout"])) {
+        if ($_GET["logout"] == 1) {
+            $auth->out();
+            header("Location: ?logout=0");
+        }
+    }
     echo "Здравствуйте, вы авторизованы!";
     echo "<br/><br/><a href='?logout=1'>Выйти</a>";
 } else {
-    ?>
-    <h3>Вход в админку</h3>
-    <form action="adminvhod.php" method="POST">
-        Логин: <input type="text" name="login" value="<?php echo (isset($_POST["login"])) ? $_POST["login"] : null; ?>"/><br><br>
-        Пароль: <input type="password" name="password" /><br><br>
-        <input type="submit" value="Войти">
-    </form>
-    <?php
+    //если не авторизован, проверяем пришёл запрос на авторизацию, или нет (пользователь просто открыл страницу)   
+    if (isset($_POST["login"]) && isset($_POST["password"])) {
+        if (!$auth->auth($_POST["login"], $_POST["password"])) {
+            echo "<h2>Логин или пароль введен не правильно!</h2>";
+            ?>
+            <h3>Вход в админку</h3>
+            <form action="adminvhod.php" method="POST">
+                Логин: <input type="text" name="login" value="<?php echo (isset($_POST["login"])) ? $_POST["login"] : null; ?>"/><br><br>
+                Пароль: <input type="password" name="password" /><br><br>
+                <input type="submit" value="Войти">
+            </form>
+            <?php
+        } else {
+            echo "Здравствуйте, вы авторизованы!";
+            echo "<br/><br/><a href='?logout=1'>Выйти</a>";
+        }
+    } else {
+        ?>
+        <h3>Вход в админку</h3>
+        <form action="adminvhod.php" method="POST">
+            Логин: <input type="text" name="login" value="<?php echo (isset($_POST["login"])) ? $_POST["login"] : null; ?>"/><br><br>
+            Пароль: <input type="password" name="password" /><br><br>
+            <input type="submit" value="Войти">
+        </form>
+        <?php
+    }
 }
 var_dump($_SESSION);
