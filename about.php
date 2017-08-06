223f595ac040c51d2cb3db7c9b9679eedb415a98
Aleksandr Golubev aka gulkinnos <gulkinnos@gmail.com>
2017-08-06 13:56:53 +0300

    Фиксим ширину

--- about.php
+++ about.php
@@ -17,48 +17,50 @@
         <script src="/resources/js/bootstrap.min.js"></script>
     </head>
     <body>
-        <div class="top-menu-block row">
-            <nav class="navbar navbar-default">
-                <div class="container-fluid"> 
-                    <div class="navbar-header">
-                        <ul class="nav navbar-nav">
-                            <li>
-                                <a href="/">Главная</a>
-                            </li>
-                            <li class="active">
-                                <a>О нас</a>
-                            </li>
-                            <li>
-                                <a href="/contacts.php">Контакты</a>
-                            </li>
-                            <li>
-                                <a href="/prices.php">Стоимость услуг</a>
-                            </li>
-                            <li>
-                                <a href="/results.php">Наши работы</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </div>
-        <h1>О мастерской velo-servis.ru</h1>
         <div class="logo col-xs-12">
-            <div class="row">
-                <div class="col-xs-1"></div>
-                <div class="col-xs-1">
-                    <img src="android-chrome-192x192.png" width="64" height="64" alt="мастерская velo-servis.ru"/>
-                </div>
-                <div class="col-xs-10"></div>
+            <div class="top-menu-block row">
+                <nav class="navbar navbar-default">
+                    <div class="container-fluid"> 
+                        <div class="navbar-header">
+                            <ul class="nav navbar-nav">
+                                <li>
+                                    <a href="/">Главная</a>
+                                </li>
+                                <li class="active">
+                                    <a>О нас</a>
+                                </li>
+                                <li>
+                                    <a href="/contacts.php">Контакты</a>
+                                </li>
+                                <li>
+                                    <a href="/prices.php">Стоимость услуг</a>
+                                </li>
+                                <li>
+                                    <a href="/results.php">Наши работы</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
             </div>
+            <h1>О мастерской velo-servis.ru</h1>
+            <div class="logo col-xs-12">
+                <div class="row">
+                    <div class="col-xs-1"></div>
+                    <div class="col-xs-1">
+                        <img src="android-chrome-192x192.png" width="64" height="64" alt="мастерская velo-servis.ru"/>
+                    </div>
+                    <div class="col-xs-10"></div>
+                </div>
+            </div>
+            <p>Уважаемые клиенты, наш сайт находится в стадии разработки.<br>
+                Скоро здесь появится много информации о нашей мастерской.</p>
+            <p>Если у вас возникли вопросы, звоните по телефону <br>+7 (495) 664-35-46</p>
+            <p>Наш адрес: Москва, Школьная улица, 23<br>
+                <a href="https://yandex.ru/maps/213/moscow/?source=wizbiz_new_map_single&text=Velo-servis.ru&ll=37.671828%2C55.745667&sll=37.671828%2C55.745667&z=17&sspn=0.016158%2C0.004805&mode=search&ol=biz&oid=156701208291&sctx=CQAAAAEAs02aainWQkCLqMIpbN9LQADg%2F%2F%2Bfi4A%2FAEAebJKuYz8AAAAABQAAAAAAAAABVgt%2BMVnMchnb1gEAAQAAgD8AAAAAAAAAAAIAAABydQ%3D%3D" target="_blank">
+                    Мы на Яндекс.Картах                
+                </a>
+            </p>
         </div>
-        <p>Уважаемые клиенты, наш сайт находится в стадии разработки.<br>
-            Скоро здесь появится много информации о нашей мастерской.</p>
-        <p>Если у вас возникли вопросы, звоните по телефону <br>+7 (495) 664-35-46</p>
-        <p>Наш адрес: Москва, Школьная улица, 23<br>
-            <a href="https://yandex.ru/maps/213/moscow/?source=wizbiz_new_map_single&text=Velo-servis.ru&ll=37.671828%2C55.745667&sll=37.671828%2C55.745667&z=17&sspn=0.016158%2C0.004805&mode=search&ol=biz&oid=156701208291&sctx=CQAAAAEAs02aainWQkCLqMIpbN9LQADg%2F%2F%2Bfi4A%2FAEAebJKuYz8AAAAABQAAAAAAAAABVgt%2BMVnMchnb1gEAAQAAgD8AAAAAAAAAAAIAAABydQ%3D%3D" target="_blank">
-                Мы на Яндекс.Картах                
-            </a>
-        </p>
     </body>
 </html>
