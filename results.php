70873aa66ab9e9e6499aabab51bc614f59f15b3c
Aleksandr Golubev aka gulkinnos <gulkinnos@gmail.com>
2017-08-06 13:54:44 +0300

    Добавляем карусель для всех фоток в папке.

--- results.php
+++ results.php
@@ -6,7 +6,9 @@
         <title>Наши работы velo-servis.ru</title>
         <link rel="shortcut icon" href="http://velo-servis.ru/favicon.ico" type="image/x-icon">
         <link href="/resources/css/bootstrap.min.css" rel="stylesheet">
-        <link href="/resources/css/custom.css" rel="stylesheet">
+        <link href="/resources/css/custom.css?1" rel="stylesheet">
+        <link href="/resources/OwlCarousel2-2.2.1/dist/assets/owl.carousel.min.css" rel="stylesheet">
+        <link href="/resources/OwlCarousel2-2.2.1/dist/assets/owl.theme.default.min.css" rel="stylesheet">
         <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
         <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
@@ -14,48 +16,77 @@
         <link rel="mask-icon" href="safari-pinned-tab.svg" color="#5bbad5">
         <script src="/resources/js/jquery-3.1.1.min.js"></script>
         <script src="/resources/js/bootstrap.min.js"></script>
+        <script src="/resources/OwlCarousel2-2.2.1/dist/owl.carousel.min.js"></script>
+        <script> $(document).ready(function () {
+                $(".owl-carousel").owlCarousel({
+                    items: 4,
+                    dots: true,
+                    nav: true,
+                    navText: ["туда", "сюда"],
+                    margin: 20,
+                    dotsEach: true,
+                    loop: true,
+                    autoplay: true,
+                    smartSpeed: 1000,
+                    autoplayTimeout: 2000,
+                    responsive: {
+                        0: {
+                            items: 1
+                        },
+                        600: {
+                            items: 2
+                        },
+                        1000: {
+                            items: 4
+                        }
+                    }
+                });
+            });
+        </script>
     </head>
     <body>
-        <div class="top-menu-block row">
-            <nav class="navbar navbar-default">
-                <div class="container-fluid"> 
-                    <div class="navbar-header">
-                        <ul class="nav navbar-nav">
-                            <li>
-                                <a href="/">Главная</a>
-                            </li>
-                            <li>
-                                <a href="/about.php">О нас</a>
-                            </li>
-                            <li>
-                                <a href="/contacts.php">Контакты</a>
-                            </li>
-                            <li>
-                                <a href="/prices.php">Стоимость услуг</a>
-                            </li>
-                            <li class="active">
-                                <a>Наши работы</a>
-                            </li>
-                        </ul>
+        <div class="col-xs-12">
+            <div class="top-menu-block row">
+                <nav class="navbar navbar-default">
+                    <div class="container-fluid"> 
+                        <div class="navbar-header">
+                            <ul class="nav navbar-nav">
+                                <li>
+                                    <a href="/">Главная</a>
+                                </li>
+                                <li>
+                                    <a href="/about.php">О нас</a>
+                                </li>
+                                <li>
+                                    <a href="/contacts.php">Контакты</a>
+                                </li>
+                                <li>
+                                    <a href="/prices.php">Стоимость услуг</a>
+                                </li>
+                                <li class="active">
+                                    <a>Наши работы</a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-            </nav>
-        </div>
-        <h1>Результаты работы мастерской velo-servis.ru</h1>
-        <div class="logo col-xs-12">
-            <div class="col-xs-4"></div>
-            <div class="col-xs-1">
-                <img src="android-chrome-192x192.png" width="64" height="64" alt="мастерская velo-servis.ru"/>
+                </nav>
             </div>
-            <div class="col-xs-7"></div>
-        </div>
-        <p>Уважаемые клиенты, наш сайт находится в стадии разработки.<br>
-            Скоро здесь появятся примеры наших работ по ремонту велосипедов, самокатов и роликовых коньков.</p>
-        <p>Если у вас возникли вопросы, звоните по телефону <br>+7 (495) 664-35-46</p>
-        <p>Наш адрес: Москва, Школьная улица, 23<br>
-            <a href="https://yandex.ru/maps/213/moscow/?source=wizbiz_new_map_single&text=Velo-servis.ru&ll=37.671828%2C55.745667&sll=37.671828%2C55.745667&z=17&sspn=0.016158%2C0.004805&mode=search&ol=biz&oid=156701208291&sctx=CQAAAAEAs02aainWQkCLqMIpbN9LQADg%2F%2F%2Bfi4A%2FAEAebJKuYz8AAAAABQAAAAAAAAABVgt%2BMVnMchnb1gEAAQAAgD8AAAAAAAAAAAIAAABydQ%3D%3D" target="_blank">
-                Мы на Яндекс.Картах                
-            </a>
-        </p>
+            <h1 align="center">Работы мастерской velo-servis.ru</h1>
+            <div class="col-xs-12" id="resultsWrapper">
+                <div class="owl-carousel owl-theme results">
+                    <?php
+                    $pictures = glob($_SERVER['DOCUMENT_ROOT'] . '/resources/images/results/*.jpg');
+                    foreach ($pictures as $picture) {
+                        ?><div class="pic"><img src="resources/images/results/<?php echo basename($picture); ?>" title="<?php echo basename($picture); ?>" alt="<?php echo basename($picture); ?>"></div><?php
+                        }
+                        ?>
+                </div>
+                <p>Если у вас возникли вопросы, звоните по телефону <br>+7 (495) 664-35-46</p>
+                <p>Наш адрес: Москва, Школьная улица, 23<br>
+                    <a href="https://yandex.ru/maps/213/moscow/?source=wizbiz_new_map_single&text=Velo-servis.ru&ll=37.671828%2C55.745667&sll=37.671828%2C55.745667&z=17&sspn=0.016158%2C0.004805&mode=search&ol=biz&oid=156701208291&sctx=CQAAAAEAs02aainWQkCLqMIpbN9LQADg%2F%2F%2Bfi4A%2FAEAebJKuYz8AAAAABQAAAAAAAAABVgt%2BMVnMchnb1gEAAQAAgD8AAAAAAAAAAAIAAABydQ%3D%3D" target="_blank">
+                        Мы на Яндекс.Картах                
+                    </a>
+                </p>
+            </div>
     </body>
 </html>
