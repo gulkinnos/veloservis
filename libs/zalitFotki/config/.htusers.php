c463c67da7c09c274b2919e546eb5ad8df2cf692
Aleksandr Golubev aka gulkinnos <gulkinnos@gmail.com>
2017-08-27 18:37:04 +0300

    Добавлен файловый менеджер.

---
+++ libs/zalitFotki/config/.htusers.php
@@ -0,0 +1,8 @@
+<?php 
+	// ensure this file is being included by a parent file
+	if( !defined( '_JEXEC' ) && !defined( '_VALID_MOS' ) ) die( 'Restricted access' );
+	$GLOBALS["users"]=array(
+	array('admin','$2a$08$94b6rkQUEWA4zWp7FPMFU.NRJpw9O7AFf/CWdLtbgW8prYvKckaCm','C:/xampp/htdocs/velo-servis.lc','http://localhost','1','','7',1),
+	array('egor','$2a$08$MS72bgFaB7cN87Tx2GDvWuNUlXy4kZH9KVeaZqenwGGYRjrRMvxJy','C:/xampp/htdocs/velo-servis.lc/resources/images/results/','http://localhost','0','','7',1),
+); 
+?>
